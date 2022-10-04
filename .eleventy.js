9aef0170919d0ab31254728acdf36adba44f5054
Artur Trifonov <wrgraff@gmail.com>
2022-10-05 01:18:48 +0500

    Setup styles and global structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,44 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addWatchTarget("./src/css/");
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('postcss-csso');
+const pimport = require('postcss-import');
+const fs = require('fs');
 
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+module.exports = function (config) {
+  config.addTemplateFormats('css');
+    config.addExtension('css', {
+        outputFileExtension: 'css',
+        compile: async (inputContent, inputPath) => {
+          if (inputPath !== './src/css/index.css') {
+            return;
+          }
+      
+          return async () => {
+            let output = await postcss([
+              pimport,
+              autoprefixer,
+              csso
+            ]).process(inputContent, { from: inputPath });
+      
+            return output.css;
+          }
+        }
+      });
+
+  config.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
-      input: "src",
-      output: "public",
+      input: 'src',
+      output: 'dist',
+      includes: '_includes',
+      data: '_data'
     },
+    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: [
+      'md', 'njk'
+    ],
   };
 };
