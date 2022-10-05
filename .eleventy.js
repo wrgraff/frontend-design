5aefe67dffd137262a5e34c3abf7e61043cc8007
Artur Trifonov <wrgraff@gmail.com>
2022-10-06 03:44:39 +0500

    Added base styles

--- .eleventy.js
+++ .eleventy.js
@@ -5,40 +5,54 @@ const pimport = require('postcss-import');
 const fs = require('fs');
 
 module.exports = function (config) {
-  config.addTemplateFormats('css');
-    config.addExtension('css', {
-        outputFileExtension: 'css',
-        compile: async (inputContent, inputPath) => {
-          if (inputPath !== './src/css/index.css') {
-            return;
-          }
-      
-          return async () => {
-            let output = await postcss([
-              pimport,
-              autoprefixer,
-              csso
-            ]).process(inputContent, { from: inputPath });
-      
-            return output.css;
-          }
-        }
-      });
 
-  config.addShortcode('year', () => `${new Date().getFullYear()}`);
+	// Styles
 
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist',
-      includes: '_includes',
-      data: '_data'
-    },
-    dataTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    templateFormats: [
-      'md', 'njk'
-    ],
-  };
+	config.addTemplateFormats('css');
+	config.addExtension('css', {
+		outputFileExtension: 'css',
+		compile: async (inputContent, inputPath) => {
+			if (inputPath !== './src/css/index.css') {
+				return;
+			}
+
+			return async () => {
+				let output = await postcss([
+					pimport,
+					autoprefixer,
+					csso
+				]).process(inputContent, { from: inputPath });
+
+				return output.css;
+			}
+		}
+	});
+
+	// Passthrough copy
+
+	[
+		'src/fonts',
+	].forEach(
+		path => config.addPassthroughCopy(path)
+	);
+
+	config.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+	// Config
+
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist',
+			includes: '_includes',
+			data: '_data'
+		},
+		dataTemplateEngine: 'njk',
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		templateFormats: [
+			'md',
+			'njk'
+		],
+	};
 };
