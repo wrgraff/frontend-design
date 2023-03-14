65f4a3f2c4a58184ecdc67cf1ebca18734e6255c
Artur Trifonov <wrgraff@gmail.com>
2023-03-15 02:33:15 +0400

    add icons and manifest

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
-  url: process.env.URL || "http://localhost:8080",
-  siteName: "Artur Trifonov's personal website",
-  siteDescription: "UI designer CV and portfolio",
-  cv: "files/cv-en.pdf"
+	url: process.env.URL || 'http://localhost:8080',
+	siteName: 'Artur Trifonov\'s personal website',
+	siteDescription: 'UI designer CV and portfolio',
+	cv: 'files/cv-en.pdf',
 };
