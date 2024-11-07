77aa8a133160004da20b88fc79cabb24a545c1be
Artur Trifonov <wrgraff@gmail.com>
2024-11-07 18:14:16 +0100

    updated cv link

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,5 +2,5 @@ module.exports = {
 	url: process.env.URL || 'http://localhost:8080',
 	siteName: 'Artur Trifonov\'s personal website',
 	siteDescription: 'UI designer CV and portfolio',
-	cv: 'files/cv-en.pdf',
+	cv: 'files/atrifonov-cv-en.pdf',
 };
