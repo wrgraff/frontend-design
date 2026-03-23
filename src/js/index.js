3a83c2bef11324e6ea10ad09c0cc222d968d477b
Artur Trifonov <hi@arturtrifonov.com>
2026-03-24 00:04:04 +0100

    Add base adoptions

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
 import { initNoJs } from './modules/no-js.js';
-// import './modules/gallery.js';
-import './modules/menu.js';
 import { initDesignSolutionSlider } from './modules/design-solution-slider.js';
-// import './modules/loader.js';
 import { initVideos } from './modules/videos.js';
 
 initNoJs();
